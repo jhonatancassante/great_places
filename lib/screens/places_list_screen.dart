575ef947e37eca8729d7633bf61e8609501081c5
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-06-11 10:21:54 -0300

    feat: created places list component

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -18,8 +20,28 @@ class PlacesListScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Center(
+        child: Consumer<GreatPlaces>(
+          child: const Center(
+            child: Text('Nenhum local cadastado.'),
+          ),
+          builder: (context, greatPlaces, child) => greatPlaces.itemsCount == 0
+              ? child!
+              : ListView.builder(
+                  itemCount: greatPlaces.itemsCount,
+                  itemBuilder: (ctx, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: FileImage(
+                        greatPlaces.itemByIndex(i).image,
+                      ),
+                    ),
+                    title: Text(
+                      greatPlaces.itemByIndex(i).title,
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+        ),
       ),
     );
   }
